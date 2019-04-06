9788ea0fb3ea7f3f080d16e71f82e35647bf5d89
xjbeta <xjbeta.tiny@gmail.com>
2019-04-06 12:28:40 +0800

    Installation guide.

---
+++ Aria2D/Views/InstallAria2cGuide/InstallationMethodViewController.swift
@@ -0,0 +1,44 @@
+//
+//  InstallationMethodViewController.swift
+//  Aria2D
+//
+//  Created by xjbeta on 2019/4/6.
+//  Copyright © 2019 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class InstallationMethodViewController: NSViewController {
+    @IBOutlet weak var tabView: NSTabView!
+    @IBAction func officeSite(_ sender: NSButton) {
+        if let u = URL(string: "https://aria2.github.io") {
+            NSWorkspace.shared.open(u)
+        }
+    }
+    
+    @IBAction func homeBrew(_ sender: NSButton) {
+        tabView.selectTabViewItem(at: 1)
+    }
+    
+    @IBAction func openTerminal(_ sender: NSButton) {
+        NSWorkspace.shared.launchApplication("Terminal")
+    }
+    
+    @IBAction func dmgFile(_ sender: NSButton) {
+        if let u = URL(string: "https://dl.devmate.com/com.aria2.aria2c/aria2c.dmg") {
+            NSWorkspace.shared.open(u)
+        }
+        tabView.selectTabViewItem(at: 2)
+    }
+    
+    @IBAction func dmgHelp(_ sender: Any) {
+        if let path = Bundle.main.path(forResource: "DMG File Installation Guide", ofType: "pdf") {
+            NSWorkspace.shared.openFile(path, withApplication: "Preview.app")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
